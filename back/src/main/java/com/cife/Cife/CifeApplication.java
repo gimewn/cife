34e8c472a2ef9c5c27f1c4f8d8e65061d9d276b9
yoonju <yoonju_1120@naver.com>
2024-01-26 10:11:26 +0900

    setting: Backend 프로젝트 세팅 #4

---
+++ back/src/main/java/com/cife/Cife/CifeApplication.java
@@ -0,0 +1,13 @@
+package com.cife.Cife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CifeApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/cife/Cife/CifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cife.Cife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
