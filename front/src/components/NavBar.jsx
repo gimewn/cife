b99146384aded41fda6ef49a5e9cfada2d22f819
yoonju <yoonju_1120@naver.com>
2024-01-30 15:49:39 +0900

    design: 로고 파일 수정 #6

--- front/src/components/NavBar.jsx
+++ front/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ export const NavBar = () => {
   return (
     <header className="border-black border-b">
       <section className="flex justify-between items-center py-7 px-12">
-        <img src={Logo} />
+        <img src={Logo} className="w-[4.6875rem]" />
         <section className="navBar_menu flex gap-6">
           <p>문화생활 관리</p>
           <p>후기 관리</p>
