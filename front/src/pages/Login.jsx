2e7c71ca170b4d7e6a739f8fe00c1a5c7f4d8657
yoonju <yoonju_1120@naver.com>
2024-01-31 17:24:06 +0900

    feat: 로그인/회원가입 UI 개발 #5

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -1,8 +1,48 @@
+import User from '@assets/user.svg';
+import Lock from '@assets/lock.svg';
+import { NavLink } from 'react-router-dom';
+import { PAGE_URL } from '@util/path';
+import { useState } from 'react';
+import Main from '@components/MainContainer';
+import LoginForm from '@components/LoginForm';
+import InputBox from '@components/InputBox';
+
 const Login = () => {
+  const [userId, setUserId] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <>
-      <h1>로그인</h1>
-    </>
+    <Main>
+      <LoginForm>
+        <h1 className="text-4xl font-bold">로그인</h1>
+        <div className="w-full flex flex-col gap-5">
+          <InputBox>
+            <img src={User} />
+            <input
+              type="text"
+              className="outline-none w-full"
+              placeholder="아이디를 입력해주세요."
+              onChange={(e) => setUserId(e.target.value)}
+            />
+          </InputBox>
+          <InputBox>
+            <img src={Lock} />
+            <input
+              type="password"
+              className="outline-none w-full"
+              placeholder="비밀번호를 입력해주세요."
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </InputBox>
+        </div>
+        <div className="flex gap-3 w-full justify-end">
+          <NavLink to={PAGE_URL.SIGNUP}>
+            <button className="btn-small bg-purple">회원가입</button>
+          </NavLink>
+          <button className="btn-small bg-red">로그인</button>
+        </div>
+      </LoginForm>
+    </Main>
   );
 };
 
--- front/src/pages/Main.jsx
+++ front/src/pages/Main.jsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+
 const Main = () => {
+  const [category, setCategory] = useState();
   return (
     <>
       <h1>메인</h1>
