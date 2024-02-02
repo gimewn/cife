9fa7be81c78a53bd605f34a7b84362651a3dba5f
yoonju <yoonju_1120@naver.com>
2024-02-02 13:45:48 +0900

    feat: React Portal과 useModal hook을 활용한 Modal 개발 #12

--- front/src/components/Home/MenuCircle.jsx
+++ front/src/components/Home/MenuCircle.jsx
@@ -3,8 +3,8 @@ const MenuCircle = ({ isFocus, image, title, onClickFun }) => {
     <div className="flex flex-col gap-5 items-center" onClick={onClickFun}>
       <div
         className={`${
-          isFocus ? 'bg-glass-full' : 'bg-glass'
-        } rounded-full p-6 hover:bg-glass-full hover:rounded-full hover:p-6 cursor-pointer`}
+          isFocus ? 'bg-focus-white' : 'bg-glass'
+        } rounded-full p-6 hover:bg-focus-white hover:rounded-full hover:p-6 cursor-pointer`}
       >
         <img src={image} className="w-16 h-16" />
       </div>
