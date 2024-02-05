8c69b8546bfe96bdb8e397f85220289feef1baf5
yoonju <yoonju_1120@naver.com>
2024-02-05 17:07:11 +0900

    feat: 문화생활 디테일 페이지 UI 틀 개발 #21

--- front/src/pages/CultureDetail.jsx
+++ front/src/pages/CultureDetail.jsx
@@ -1,9 +1,14 @@
 import { useParams } from 'react-router-dom';
 
+import Main from '@components/MainContainer';
+
+import { DUMMY_DATA } from '@util/variable';
+
 const CultureDetail = () => {
   const params = useParams();
+  const data = DUMMY_DATA[params.cultureId - 1];
 
-  return <>{params.cultureId}</>;
+  return <Main></Main>;
 };
 
 export default CultureDetail;
