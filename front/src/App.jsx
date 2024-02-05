243fd45d988e30cfd2ee91be755f9313e84e0cf9
yoonju <yoonju_1120@naver.com>
2024-02-05 16:47:54 +0900

    feat: 문화생활 생성/수정 페이지 UI 개발 #15

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,27 @@
+import { useEffect } from 'react';
 import Router from './Router';
 
 function App() {
+  useEffect(() => {
+    console.log(`...Λ＿Λ
+    （ㆍωㆍ)つ━☆*。
+    ⊂　　 ノ 　　　.카
+    　し-Ｊ　　　°。이 *´¨)
+    　　　　　　..　.· ´¸.·프*´¨) ¸.·*¨)
+    　　　　　　　　　　(¸.·´ (¸.'*
+    `);
+
+    console.log(`＼척추 수술 2천만원... 허리 펴...／
+    　　ｏ
+    　　 。
+    　　　｡
+    　　∧＿∧
+    　 (*　･ω･)
+    ＿(__つ/￣￣￣/_
+    　　＼/　　　/
+    `);
+  }, []);
+
   return (
     <>
       <Router />
