af0bfd87e5a72e9817ee79e609da2f4c23d0d6e4
yoonju <yoonju_1120@naver.com>
2024-02-06 17:00:38 +0900

    setting: DB 테이블 생성 & DB - MyBatis 연동

---
+++ back/src/main/java/com/cife/Cife/dto/UserDTO.java
@@ -0,0 +1,11 @@
+package com.cife.Cife.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UserDTO {
+}
---
+++ back/src/main/java/com/cife/Cife/service/UserService.java
@@ -0,0 +1,9 @@
+package com.cife.Cife.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+}
