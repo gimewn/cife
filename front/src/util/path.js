39be47473ba7c783764707c2c5b74e9071c9f828
gimewn <yoonju_1120@naver.com>
2024-02-12 17:56:28 +0900

    feat: API URL 상수화 #39

--- front/src/util/path.js
+++ front/src/util/path.js
@@ -10,3 +10,22 @@ export const PAGE_URL = {
   CULTURE_EDIT: '/culture/edit',
   REVIEW_EDIT: '/review/edit',
 };
+
+export const BASE_URL = 'http://localhost:8080';
+
+export const API_URL = {
+  SIGNUP: BASE_URL + '/signup',
+  LOGIN: BASE_URL + '/login',
+  LOGOUT: BASE_URL + '/logout',
+  HOME_SEE_LIST: BASE_URL + '/seeList',
+  HOME_RESERVATION: BASE_URL + '/reservation',
+  NOT_REVIEWED_LIST: BASE_URL + '/notReviewedList',
+  MONTHLY: (year, month) => `${BASE_URL}/monthly?year=${year}&month=${month}`,
+  CULTURE_RUD: (cultureId) => `${BASE_URL}/culture?cultureId=${cultureId}`,
+  CULTURE_CREATE: BASE_URL + '/culture',
+  CULTURE_LIST: BASE_URL + '/culture/all',
+  RESERVATION_UD: (cultureId) => `${BASE_URL}/culture/reservation?cultureId=${cultureId}`,
+  REVIEW_CREATE: BASE_URL + '/review',
+  REVIEW_RUD: (reviewId) => `${BASE_URL}/review?reviewId=${reviewId}`,
+  REVIEW_LIST: BASE_URL + '/review/all',
+};
